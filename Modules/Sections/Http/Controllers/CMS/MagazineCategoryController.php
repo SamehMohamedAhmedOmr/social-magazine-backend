a094bb7228b92fc68f88effb3bc60e09f876728d
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2020-10-24 05:16:22 +0200

    frontend controllers

--- Modules/Sections/Http/Controllers/CMS/MagazineCategoryController.php
+++ Modules/Sections/Http/Controllers/CMS/MagazineCategoryController.php
@@ -2,78 +2,71 @@
 
 namespace Modules\Sections\Http\Controllers\CMS;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Modules\Base\Requests\PaginationRequest;
+use Modules\Sections\Http\Requests\CMS\MagazineCategoryRequest;
+use Modules\Sections\Services\CMS\MagazineCategoryService;
+use Throwable;
 
 class MagazineCategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
+    private $service;
+
+    public function __construct(MagazineCategoryService $service)
     {
-        return view('sections::index');
+        $this->service = $service;
     }
 
     /**
-     * Show the form for creating a new resource.
-     * @return Response
+     * Display a listing of the resource.
+     * @param PaginationRequest $request
+     * @return JsonResponse|void
      */
-    public function create()
+    public function index(PaginationRequest $request)
     {
-        return view('sections::create');
+        return $this->service->index();
     }
+
 
     /**
      * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Response
+     * @param MagazineCategoryRequest $request
+     * @return JsonResponse
+     * @throws Throwable
      */
-    public function store(Request $request)
+    public function store(MagazineCategoryRequest $request)
     {
-        //
+        return $this->service->store($request);
     }
 
     /**
      * Show the specified resource.
-     * @param int $id
-     * @return Response
+     * @param MagazineCategoryRequest $request
+     * @return JsonResponse|void
      */
-    public function show($id)
+    public function show(MagazineCategoryRequest $request)
     {
-        return view('sections::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('sections::edit');
+        return $this->service->show($request->magazine_category);
     }
 
     /**
      * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
+     * @param MagazineCategoryRequest $request
+     * @return JsonResponse|void
      */
-    public function update(Request $request, $id)
+    public function update(MagazineCategoryRequest $request)
     {
-        //
+        return $this->service->update($request->magazine_category, $request);
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
+     * @param MagazineCategoryRequest $request
+     * @return JsonResponse|void
      */
-    public function destroy($id)
+    public function destroy(MagazineCategoryRequest $request)
     {
-        //
+        return $this->service->delete($request->magazine_category);
     }
 }
--- Modules/Sections/Http/Controllers/CMS/TestimonialController.php
+++ Modules/Sections/Http/Controllers/CMS/TestimonialController.php
@@ -2,78 +2,71 @@
 
 namespace Modules\Sections\Http\Controllers\CMS;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Modules\Base\Requests\PaginationRequest;
+use Modules\Sections\Http\Requests\CMS\TestimonialRequest;
+use Modules\Sections\Services\CMS\TestimonialService;
+use Throwable;
 
 class TestimonialController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
+    private $service;
+
+    public function __construct(TestimonialService $service)
     {
-        return view('sections::index');
+        $this->service = $service;
     }
 
     /**
-     * Show the form for creating a new resource.
-     * @return Response
+     * Display a listing of the resource.
+     * @param PaginationRequest $request
+     * @return JsonResponse|void
      */
-    public function create()
+    public function index(PaginationRequest $request)
     {
-        return view('sections::create');
+        return $this->service->index();
     }
+
 
     /**
      * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Response
+     * @param TestimonialRequest $request
+     * @return JsonResponse
+     * @throws Throwable
      */
-    public function store(Request $request)
+    public function store(TestimonialRequest $request)
     {
-        //
+        return $this->service->store($request);
     }
 
     /**
      * Show the specified resource.
-     * @param int $id
-     * @return Response
+     * @param TestimonialRequest $request
+     * @return JsonResponse|void
      */
-    public function show($id)
+    public function show(TestimonialRequest $request)
     {
-        return view('sections::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('sections::edit');
+        return $this->service->show($request->testimonial);
     }
 
     /**
      * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
+     * @param TestimonialRequest $request
+     * @return JsonResponse|void
      */
-    public function update(Request $request, $id)
+    public function update(TestimonialRequest $request)
     {
-        //
+        return $this->service->update($request->testimonial, $request);
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
+     * @param TestimonialRequest $request
+     * @return JsonResponse|void
      */
-    public function destroy($id)
+    public function destroy(TestimonialRequest $request)
     {
-        //
+        return $this->service->delete($request->testimonial);
     }
 }

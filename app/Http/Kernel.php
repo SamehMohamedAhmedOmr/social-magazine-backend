c4dc9b070f5b309d1c83de4d8121339dcab2a310
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2020-10-30 03:00:48 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\Cors::class
     ];
 
     /**
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Modules\Base\Middleware\SetLanguage::class,
-            \Fruitcake\Cors\HandleCors::class,
             \App\Http\Middleware\Cors::class
             // \Modules\ACL\Http\Middleware\PermissionMiddleware::class,
         ],

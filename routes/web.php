7d7b6c37c5ad2b66baff5c41d0d096c08a33b7cd
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2020-11-06 18:48:27 +0200

    add route to thumb

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,22 @@ Route::get('storage/{folder}/{subFolder}/{image}', function ($folder, $subFolder
 
     return $response;
 });
+
+Route::get('storage/{folder}/{subFolder}/{thumb}/{image}', function ($folder, $subFolder, $thumb, $image)
+{
+    $filename = 'storage' .'/'. $folder .'/'. $subFolder . '/' .  $thumb . '/' . $image;
+    $path = public_path($filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});

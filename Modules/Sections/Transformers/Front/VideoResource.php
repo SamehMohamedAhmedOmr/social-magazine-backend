adba6d35430da3bdbc4824c1c47b8ead0f9f18e6
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-01-07 19:50:09 +0200

    Resource

---
+++ Modules/Sections/Transformers/Front/VideoResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Sections\Transformers\Front;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\Resource;
+use Modules\Gallery\Transformers\Frontend\GalleryResource;
+
+class VideoResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'link' => $this->link,
+            'images' => GalleryResource::collection($this->whenLoaded('images')),
+            'created_at' => $this->created_at
+        ];
+    }
+}

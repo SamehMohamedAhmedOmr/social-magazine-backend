a8a515b2fc8cb52ec1014d92d342a2667128e160
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-07-17 13:21:15 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,41 +11,41 @@
 |
 */
 
-
-Route::get('storage/{folder}/{subFolder}/{image}', function ($folder, $subFolder, $image)
-{
-    $filename = 'storage' .'/'. $folder .'/'. $subFolder . '/' . $image;
-    $path = public_path($filename);
-
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
-});
-
-Route::get('storage/{folder}/{subFolder}/{thumb}/{image}', function ($folder, $subFolder, $thumb, $image)
-{
-    $filename = 'storage' .'/'. $folder .'/'. $subFolder . '/' .  $thumb . '/' . $image;
-    $path = public_path($filename);
-
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
-});
+//
+//Route::get('storage/{folder}/{subFolder}/{image}', function ($folder, $subFolder, $image)
+//{
+//    $filename = 'storage' .'/'. $folder .'/'. $subFolder . '/' . $image;
+//    $path = public_path($filename);
+//
+//    if (!File::exists($path)) {
+//        abort(404);
+//    }
+//
+//
+//    $file = File::get($path);
+//    $type = File::mimeType($path);
+//
+//    $response = Response::make($file, 200);
+//    $response->header("Content-Type", $type);
+//
+//    return $response;
+//});
+//
+//Route::get('storage/{folder}/{subFolder}/{thumb}/{image}', function ($folder, $subFolder, $thumb, $image)
+//{
+//    $filename = 'storage' .'/'. $folder .'/'. $subFolder . '/' .  $thumb . '/' . $image;
+//    $path = public_path($filename);
+//
+//    if (!File::exists($path)) {
+//        abort(404);
+//    }
+//
+//
+//    $file = File::get($path);
+//    $type = File::mimeType($path);
+//
+//    $response = Response::make($file, 200);
+//    $response->header("Content-Type", $type);
+//
+//    return $response;
+//});
